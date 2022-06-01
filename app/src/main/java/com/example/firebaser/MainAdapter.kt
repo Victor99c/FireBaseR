133089c17f005e42856894ab0d6464727ec18ff5
Victor99c <cova981015@gmail.com>
2022-06-01 10:32:13 -0500

    Reto

--- app/src/main/java/com/example/firebaser/MainAdapter.kt
+++ app/src/main/java/com/example/firebaser/MainAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.firebaser
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -14,18 +15,22 @@ class MainAdapter(private val peliculas: JSONArray): RecyclerView.Adapter<MainAd
     }
 
     override fun onBindViewHolder(holder: MainAdapter.MainHolder, position: Int){
-       if(peliculas[position].toString()!="null")
-        holder.render(peliculas[position] as JSONObject)
+        if(peliculas[position].toString() == "null"){
+            Log.e("Hola","Entra")
+        }else{
+            holder.render(peliculas[position] as JSONObject)
+        }
     }
 
     override fun getItemCount(): Int = peliculas.length()
+
     class MainHolder(val binding: ItemMovieBinding): RecyclerView.ViewHolder(binding.root){
         fun render(movie: JSONObject){
-            binding.tvMovieTitle.setText(movie.getString("titulo"))
-            binding.tvMovieCountry.setText(movie.getString("pais"))
-            binding.tvMovieGenre.setText(movie.getString("genero"))
-            binding.tvMovieYear.setText(movie.getString("año"))
-            binding.ivMovie.setImageResource(R.drawable.movie_item)
+            binding.ivTitulo.setText(movie.getString("titulo"))
+            binding.ivPais.setText(movie.getString("pais"))
+            binding.ivGenero.setText(movie.getString("genero"))
+            binding.ivAo.setText(movie.getString("ao"))
+            binding.ivCartel.setImageResource(R.drawable.movie_item)
         }
     }
 }
